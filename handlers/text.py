c81adc5fe705a04529a3dc4a8a5ab43c2e182c7d
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-12-25 18:54:11 +0300

    add new functions which reacting for menu calls

--- handlers/text.py
+++ handlers/text.py
@@ -6,9 +6,15 @@ from loader import dp
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(msg: types.Message):
    if msg.text.startswith("Расписание"):
-       await schedule.start_cmd_handler(msg)
+       await schedule.start_schedule_handler(msg)
    elif msg.text.startswith("Помощь"):
         await help.help_command(msg)
+   elif msg.text.startswith("Расположение"):
+       await hall_location.start_location_handler(msg)
+   elif msg.text.startswith("Тренера"):
+       await coaches.coaches_index(msg)
+   elif msg.text.startswith("Прайс"):
+        await price_list.price_list_handler(msg)
    elif msg.text.lower() == 'привет':
        await msg.answer('Привет!')
    else:
