d393cf885f38410083c770e31b34d34457aa5804
MyNameIsMaxwell <makslyubchuk@mail.ru>
2022-12-25 18:51:38 +0300

    add inline keyboard with back communication link

--- handlers/help.py
+++ handlers/help.py
@@ -6,4 +6,12 @@ from config_data.config import DEFAULT_COMMANDS
 @dp.message_handler(commands=['help'])
 async def help_command(msg: types.Message):
 	text = [f'/{command} - {desk}' for command, desk in DEFAULT_COMMANDS if command != 'help']
-	await msg.reply('\n'.join(text))
+	keyboard_markup = types.InlineKeyboardMarkup()
+	keyboard_markup.add(types.InlineKeyboardButton("Что-то не работает?", callback_data="my_contact"))
+	keyboard_markup.add(types.InlineKeyboardButton("Дайте мне знать, пожалуйста🙂", url="https://t.me/Lymaxe"))
+	await msg.reply('\n'.join(text), reply_markup=keyboard_markup)
+
+
+@dp.callback_query_handler(lambda c: c.data.startswith('my_contact'))
+async def inline_schedule_answer_callback_handler(query: types.CallbackQuery):
+	await query.answer('Ссылка под этой кнопкой)')
