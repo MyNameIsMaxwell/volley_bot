912f09f8abd941d3a5bf7411f5ca2232ee22a9d0
MyNameIsMaxwell <makslyubchuk@mail.ru>
2023-01-05 12:12:05 +0300

    text changes

--- handlers/text.py
+++ handlers/text.py
@@ -3,19 +3,20 @@ from handlers import *
 
 from loader import dp
 
+
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(msg: types.Message):
-   if msg.text.startswith("Расписание"):
+    if msg.text.startswith("Расписание"):
        await schedule.start_schedule_handler(msg)
-   elif msg.text.startswith("Помощь"):
+    elif msg.text.startswith("Помощь"):
         await help.help_command(msg)
-   elif msg.text.startswith("Расположение"):
+    elif msg.text.startswith("Расположение"):
        await hall_location.start_location_handler(msg)
-   elif msg.text.startswith("Тренера"):
+    elif msg.text.startswith("Тренера"):
        await coaches.coaches_index(msg)
-   elif msg.text.startswith("Прайс"):
+    elif msg.text.startswith("Прайс"):
         await price_list.price_list_handler(msg)
-   elif msg.text.lower() == 'привет':
-       await msg.answer('Привет!')
-   else:
-       await msg.answer('A?')
+    elif msg.text.lower() == 'привет':
+        await msg.answer('Привет!')
+    else:
+        await msg.answer('Пока что дальше этих /help команд я не понимаю🤷‍♂️')
