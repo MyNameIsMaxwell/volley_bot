1c93ce7faf4aa6d09c401ea24795f1cf28f8a3b9
Max <makslyubchuk00@mail.ru>
2023-02-07 12:46:22 +0300

    send message by command taking users from google sheet and add trial lesson button under message

--- handlers/sender.py
+++ handlers/sender.py
@@ -2,14 +2,21 @@ from aiogram import types
 
 from loader import bot, dp
 from config_data import config
-from database import datawork
+from get_user_info import google_sheet_parse
+from . import first_connection
 
 
 @dp.message_handler(commands=['sendall'], user_id=config.ADMINS)
 async def sender(message: types.Message):
-    if message.from_user.id in config.ADMINS:
+    if str(message.from_user.id) in config.ADMINS:
+        keyboard_markup = types.InlineKeyboardMarkup(row_width=1)
+        keyboard_markup.add(types.InlineKeyboardButton(text='Записаться на пробное!😍', callback_data='trial_lesson_on'))
         text = message.text[9:]
-        users = datawork.get_users_for_message()
+        users = google_sheet_parse.get_newbie_users_info()[0]
         for user in users:
-            await bot.send_message(user.user_id, text)
+            await bot.send_message(user, text, reply_markup=keyboard_markup)
 
+
+@dp.callback_query_handler(lambda c: c.data.endswith('trial_lesson_on'))
+async def inline_trial_callback_handler(query: types.CallbackQuery):
+    await first_connection.first_connection_handler(query.message)
