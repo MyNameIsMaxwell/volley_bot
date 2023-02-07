111730cedf13b906c0ee405d1323c76666d191d0
Max <makslyubchuk00@mail.ru>
2023-02-07 12:43:51 +0300

    Start scheduler

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from aiogram import executor
 
-from loader import dp
-import handlers
+from loader import dp, scheduler
+
 
 if __name__ == '__main__':
+   scheduler.start()
    executor.start_polling(dp, skip_updates=True)
