74b1c007020224f32e909a0cb4d7ca38abc9df8e
Max <makslyubchuk00@mail.ru>
2023-02-07 12:49:25 +0300

    add some function which answer at relevant words for new users

--- handlers/text.py
+++ handlers/text.py
@@ -1,15 +1,21 @@
 from aiogram import types
+
 from handlers import *
 
 from loader import dp
 from database import datawork
+from get_user_info import google_sheet_parse
 
 
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(msg: types.Message):
-    if await datawork.get_user_status(message=msg) is True:
+    if str(msg.from_user.id) in google_sheet_parse.get_newbie_users_info()[0]:
         if msg.text.startswith("Записаться на"):
             await first_connection.first_connection_handler(msg)
+        elif msg.text.startswith("Расписание"):
+            await schedule.start_schedule_handler(msg)
+        elif msg.text.startswith("О нас"):
+            await about_us.about_us_handler(msg)
         else:
             await msg.answer("Хм?")
     else:
