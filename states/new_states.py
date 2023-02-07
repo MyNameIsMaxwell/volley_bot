27c4d3a695f90abc19e6c522e6fe7a86bc0753f6
Max <makslyubchuk00@mail.ru>
2023-02-07 12:44:35 +0300

    add some states

--- states/new_states.py
+++ states/new_states.py
@@ -2,4 +2,8 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
 class GreetingsStates(StatesGroup):
-    VladGreetings = State()
+    lets_go = State()
+    training_interest = State()
+    interest_yes = State()
+    expensive = State()
+
